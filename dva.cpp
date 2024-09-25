06f7c7004c96f84beb89a3b525f6f56c118f0c22
unlund <64699292+unlund@users.noreply.github.com>
2024-09-25 12:13:10 +0200

    Add files via upload

---
+++ dva.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int a;
+    int h;
+    cout << "Основание: "; cin >> a;
+    cout << "Высота: "; cin >> h;
+
+    cout << "Площадь = " << (h * a) / 2;
+}
