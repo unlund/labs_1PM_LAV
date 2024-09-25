49d136183f39b8d72d21d964b964a41b011ef66d
unlund <64699292+unlund@users.noreply.github.com>
2024-09-25 13:09:44 +0200

    Add files via upload

---
+++ dva.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    unsigned int x;
+    cout << "X: "; cin >> x;
+    bitset<32> binx(x);
+    cout << binx << "\n";
+    unsigned int i;
+    cout << "i: "; cin >> i;
+
+    unsigned int savex = (x << 32 - (i - 1))>>32 - (i-1);
+
+    unsigned int res = ((x >> i) << i) + savex;
+    bitset<32> binr(res);
+    cout << res << "\n" << binr;
+}
